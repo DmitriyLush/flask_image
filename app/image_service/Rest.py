1448fee155d1be24d3104fe8975fe9130567d3e0
DmitriyLush <aorti0712@gmail.com>
2021-09-06 07:55:45 +0300

    hello

---
+++ app/image_service/Rest.py
@@ -0,0 +1,79 @@
+from flask import Flask, request, send_from_directory, jsonify
+from PIL import Image
+from io import BytesIO
+import os
+import time
+import base64
+import random
+import string
+
+app = Flask(__name__)
+
+
+def generate_random_name(length):
+    """В Image.save() необходим обязательный аргумент с сохраняемым именем файла, если его указать прямо,
+    то больше одной картинки сохранить невозможно. Для решения этой проблемы написана функция, генерирующая рандомные
+    имена."""
+    letters = string.ascii_lowercase
+    rand_string = ''.join(random.choice(letters) for i in range(length))
+    return rand_string
+
+
+@app.route('/')
+def hello():
+    return jsonify('Hello!')
+
+
+@app.route('/image', methods=['GET'])
+def get_images_info():
+    """Выводит список изображений в JSON формате который содержит имя файла, размер, время последнего изменения."""
+    res = {}
+    for i in os.listdir('images'):
+        res[i] = [time.ctime(os.path.getmtime(os.path.abspath(os.path.join('images', i)))),
+                  str(os.stat(os.path.abspath(os.path.join('images', i))).st_size / 1024) + ' kilobytes']
+    return jsonify(res)
+
+
+@app.route('/image', methods=['POST'])
+def base64_upload():
+    """Создает новое изображение из переданной base64 строки.
+    example:
+    endpoint: http://<hostname>:<port>/image
+    POST JSON {"file": "your_base64_string"}"""
+    try:
+        file = request.get_json()
+        data = file.get('file')
+        im = Image.open(BytesIO(base64.b64decode(data))).convert('RGB')
+        filename = generate_random_name(3) + '.jpg'
+        im.save(os.path.join('images', filename), 'JPEG')
+        return jsonify(f"file {filename} added to 'images' folder")
+    except Exception:
+        return jsonify('Incorrect JSON or base64 string.')
+
+
+@app.route('/image', methods=['DELETE'])
+def delete_image():
+    """Удаляет изображение по его имени.
+    example:
+    http://<hostname>:<port>/image?filename=<filename>.jpg"""
+    try:
+        filename = request.args.get('filename')
+        os.remove(os.path.join('images', filename))
+        return jsonify(f" file {filename} has been deleted")
+    except Exception:
+        return jsonify('Incorrect filename in args.')
+
+
+@app.route('/images/<filename>', methods=['GET'])
+def image_preview(filename):
+    """Отдает изображения из images по эндпоинту.
+     example:
+     http://<hostname>:<port>/images/<filename>.jpg"""
+    try:
+        return send_from_directory('images', filename)
+    except Exception:
+        return jsonify('Incorrect filename.')
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
